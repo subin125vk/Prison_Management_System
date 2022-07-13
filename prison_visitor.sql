bf948fea7b619c67d767345e21cd029e5d2a7913
subin125vk <87141152+subin125vk@users.noreply.github.com>
2022-07-13 10:16:43 +0530

    Add files via upload

---
+++ prison_visitor.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 10.4.24-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `visitor` (
+	`visitorid` int (11),
+	`fullname` varchar (765),
+	`prisonervisited` varchar (765),
+	`phonenumber` varchar (765),
+	`timeofvisit` varchar (765),
+	`address` varchar (765),
+	`gender` varchar (765)
+); 
+insert into `visitor` (`visitorid`, `fullname`, `prisonervisited`, `phonenumber`, `timeofvisit`, `address`, `gender`) values('123','sub','sub','1234567980','12:12','asdfgrewe','on');
---
+++ staff.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 10.4.24-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `staff` (
+	`fullname` varchar (150),
+	`age` int (11),
+	`department` varchar (90),
+	`staff_id` varchar (60),
+	`phonenumber` varchar (30),
+	`address` varchar (180),
+	`gender` varchar (60)
+); 
+insert into `staff` (`fullname`, `age`, `department`, `staff_id`, `phonenumber`, `address`, `gender`) values('sdsd','25','asxas','sdas','9564112136','asxas','on');
