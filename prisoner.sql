b7f603844537af6245d15d25a44e0015ac5b0aa7
subin125vk <87141152+subin125vk@users.noreply.github.com>
2022-07-22 22:04:13 +0530

    Add files via upload

---
+++ prisoner.sql
@@ -0,0 +1,27 @@
+/*
+SQLyog Community v13.1.7 (64 bit)
+MySQL - 10.4.24-MariaDB 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `prisoner` (
+	`id` int (11),
+	`fullname` varchar (765),
+	`age` int (11),
+	`prisoner_id` varchar (765),
+	`casenumber` varchar (765),
+	`edate` varchar (765),
+	`rdate` varchar (765),
+	`etime` varchar (765),
+	`cdetail` varchar (765),
+	`address` varchar (765),
+	`econtact` varchar (765),
+	`gender` varchar (765)
+); 
+insert into `prisoner` (`id`, `fullname`, `age`, `prisoner_id`, `casenumber`, `edate`, `rdate`, `etime`, `cdetail`, `address`, `econtact`, `gender`) values('1','subin','23','asd123','assdfgqwer','2022-07-20','2022-07-12','09:24','zxaxz','zxczx','13212131313','Male');
+insert into `prisoner` (`id`, `fullname`, `age`, `prisoner_id`, `casenumber`, `edate`, `rdate`, `etime`, `cdetail`, `address`, `econtact`, `gender`) values('2',NULL,'0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `prisoner` (`id`, `fullname`, `age`, `prisoner_id`, `casenumber`, `edate`, `rdate`, `etime`, `cdetail`, `address`, `econtact`, `gender`) values('3',NULL,'0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `prisoner` (`id`, `fullname`, `age`, `prisoner_id`, `casenumber`, `edate`, `rdate`, `etime`, `cdetail`, `address`, `econtact`, `gender`) values('4',NULL,'0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `prisoner` (`id`, `fullname`, `age`, `prisoner_id`, `casenumber`, `edate`, `rdate`, `etime`, `cdetail`, `address`, `econtact`, `gender`) values('5',NULL,'0',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+insert into `prisoner` (`id`, `fullname`, `age`, `prisoner_id`, `casenumber`, `edate`, `rdate`, `etime`, `cdetail`, `address`, `econtact`, `gender`) values('6','asd','25','asd','asd12','2022-07-22','2022-07-22','11:25','sda','asd','9565651242','Male');
